5d7aa3321f34b54312bb1fc6ecac375c4470e649
Rohit Shrestha <rohits2013@gmail.com>
2022-02-23 17:04:33 -0700

    Test for App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>IG_Clone</Text>
+      <Text>IG_Clone v0.1</Text>
       <StatusBar style="auto" />
     </View>
   );
