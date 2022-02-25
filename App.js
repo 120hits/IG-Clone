9bb737b2e2edf826ddc34d7ffeec40984873bde7
Rohit Shrestha <rohits2013@gmail.com>
2022-02-24 17:38:45 -0700

    git hub test

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>IG_Clone v0.11</Text>
+      <Text>IG_Clone github test</Text>
       <StatusBar style="auto" />
     </View>
   );
