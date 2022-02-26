ea756c84f015a626f9998cb9de87f33a46977044
Rohit Shrestha <rohits2013@gmail.com>
2022-02-25 18:28:17 -0700

    Add files via upload

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import { NavigationContainer } from '@react-navigation/native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>IG_Clone github test</Text>
+      <Text>IG_Clone github test 11</Text>
       <StatusBar style="auto" />
     </View>
   );
