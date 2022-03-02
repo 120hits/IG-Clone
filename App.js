600d81b7ce3477348e5d0da4c9750e88112743f3
Rohit Shrestha <rohits2013@gmail.com>
2022-03-01 19:18:36 -0700

    Add files via upload

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>IG_Clone github test 11</Text>
+      <Text>git repo added testt 1.1</Text>
       <StatusBar style="auto" />
     </View>
   );
